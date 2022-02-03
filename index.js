d0d49edb57369a0ee645aeb4bc5cfa67120321d4
Scott Yeskie <scott@yeskie.com>
2022-02-02 23:22:27 -0600

    Add rough code (wip)
    Fails on socket. Might want to try `node-ipc`

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-// Library to send signal to Q keyboards
-const q = require('daskeyboard-applet');
+import * as q from 'daskeyboard-applet';
+import * as unix from 'unix-dgram';
+import { xdgRuntime } from 'xdg-basedir';
 
 const logger = q.logger;
 
-const SOCKET = "$XDG_RUNTIME_DIR/yubikey-touch-detector.socket";
+const YTD_SOCKET_NAME = 'yubikey-touch-detector.socket';
 
 class DasYubiTouch extends q.DesktopApp {
     constructor() {
         super();
 
-        this.pollingInterval = 3000;
-        logger.info("DAS Keyboard touch ready");
+        this.server = unix.createSocket('unix_dgram', this.updateTouch);
+
+        this.server.bind(xdgRuntime + '/' + YTD_SOCKET_NAME);
+
+        logger.info('DAS YubiTouch ready');
+    }
+
+    signalClear() {
+        this.signal(new q.Signal({
+            points: [new q.Point(this.config.defaultColor)]
+        }));
+    }
+
+    signalNeedTouch() {
+        this.signal(new q.Signal({
+            points: [new q.Point(this.config.waitColor, q.Effects.BLINK)]
+        }));
+    }
+
+    async updateTouch(buf) {
+        console.log('Received ' + buf);
+        // So should get GPG_1, GPG_0, U2F_1, U2F_0
+        // 0 events are stop wait for touch
+        // 1 events are start wait for touch
+    }
+
+    shutdown() {
+        this.server.close();
     }
 }
+
+// module.exports = {
+//     DasYubiTouch: DasYubiTouch
+// };
+
+// daskeyboard-applet indicates that needs to instantiate
+// eslint-disable-next-line no-unused-vars
+const dasYubiTouch = new DasYubiTouch();
